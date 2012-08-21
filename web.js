92b10c570b3fd444c54822af5aad27386cc301de
Jo Jae-Yound <astatin@astr.co.kr>
2012-08-21 16:26:15 +0900

    New

--- web.js
+++ web.js
@@ -1,10 +1,5 @@
 // 모듈 추출
-var http = require('http');
+var http = require('os');
 
-// 웹서버 생성 및 실행
-http:createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type':'text/html'});
-	response.end('<h1>Hello World .. !</h1>');
-}).listen(8001, function () {
-	console.log('Server running at ~');
-});
+console.log(os.hostname());
+console.log(os.type());
